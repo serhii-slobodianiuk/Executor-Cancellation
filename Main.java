8932612167ed7e01e0cecf72468d9081e78562d5
serhii-slobodianiuk <48651384+serhii-slobodianiuk@users.noreply.github.com>
2019-06-12 01:27:47 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -77,17 +77,3 @@ public class Main {
 
     }
 }
-
-class ConsoleLogger implements CopyFilesBuiltInBufferedTask.Observer {
-
-    @Override
-    public void onProgressUpdated(long receivedBytes, long totalBytes) {
-        System.out.println(String.format("Progress: %d/%d", receivedBytes, totalBytes));
-    }
-
-    @Override
-    public void onCopyingCompleted(File outputFile) {
-        System.out.println();
-        System.out.println(String.format("Copying has completed to %s", outputFile.getAbsolutePath()));
-    }
-}
